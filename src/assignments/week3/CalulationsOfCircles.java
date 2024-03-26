71428097fcdc76872b2d884a54c16d406565f187
Michael_Martucci <michael.martucci@yahoo.com>
2024-03-26 17:46:25 -0400

    added week 3 day 2 assignment

---
+++ src/assignments/week3/CalulationsOfCircles.java
@@ -0,0 +1,27 @@
+package assignments.week3;
+
+public class CalulationsOfCircles {
+
+    public static void main (String ... args){
+        // create object circle 1
+        Circle circle1 = new Circle();
+        circle1.setRadius(5);
+        circle1.getArea();
+        circle1.getDiameter();
+        circle1.getCircumference();
+        System.out.println("> Circle 1 properties");
+        System.out.println(circle1); //trim white space
+        System.out.println("////////////////////");
+
+        Circle circle2 = new Circle();
+        circle2.setRadius(0);
+        circle2.getArea();
+        circle2.getDiameter();
+        circle2.getCircumference();
+        System.out.println("> Circle 2 properties");
+        System.out.println("////////////////////");
+        System.out.println(circle2);
+
+    }
+}
+    
---
+++ src/assignments/week3/Circle.java
@@ -0,0 +1,49 @@
+package assignments.week3;
+
+// declare class and instance variables
+public class Circle {
+
+    private double radius = 0;
+
+    // instance no args
+    public Circle() {
+    }
+
+    // constructor with paramteres
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // set radius of circle
+    public void setRadius(double radius) {
+        this.radius = radius;
+
+    }
+
+    // equation to calcualate area
+    public double getArea() {
+
+
+        return Math.PI * Math.pow(radius, 2);
+
+    }
+
+    // equation to calculate diameter
+    public double getDiameter() {
+        return 2 * radius;
+
+    }
+
+    // equation to calculate circumference
+    public double getCircumference() {
+
+        return 2 * Math.PI * radius;
+    }
+
+    // override print statement so outputs as string
+    @Override
+    public String toString() {
+        return "this circle has an area of:" + Math.PI * Math.pow(radius, 2) + "\n the diameter is: " + 2 * radius +
+                "\n and the circumference is: " + 2 * Math.PI * radius;
+    }
+}
