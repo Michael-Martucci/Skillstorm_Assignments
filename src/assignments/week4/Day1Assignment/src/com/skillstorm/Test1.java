f31362d3b9c9677826f1c629e093a45389891413
Michael_Martucci <michael.martucci@yahoo.com>
2024-04-01 16:18:03 -0400

    week4 daily assignment added

---
+++ src/assignments/week4/Day1Assignment/src/com/skillstorm/Test1.java
@@ -0,0 +1,17 @@
+package com.skillstorm;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Flower flower1 = new Rose("pink", "Damask", true); 
+        flower1.smell();
+    }
+
+}
+//Test 1 will compile and it will run. 
+//it will print "This pink damask rose smells good"
+// In polymorphism this is an example of constructor chaining and method overriding
+/*  constructor chaining since the constructor uses the super keyword to call the constructor
+from the parent class Flower and use the arguments for color and name*/
+/*method overrding since test 1 over rides the print statement by accessing its own boolean
+ * property to determine if the flower is in bloom or not and then return the associated description
+ */
